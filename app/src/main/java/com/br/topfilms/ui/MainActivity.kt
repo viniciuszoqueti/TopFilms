860d6d6b7620b62b57a7873e250007a05f2cfaf2
viniciuszoqueti <vinicius.zoqueti@hotmail.com>
2022-07-15 02:06:14 -0300

    first commit

---
+++ app/src/main/java/com/br/topfilms/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.br.topfilms.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.topfilms.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
