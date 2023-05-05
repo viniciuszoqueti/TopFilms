2e6d3104c5916c0f52d7ad0a48b3bcb20f1e7856
vinicius zoqueti <vinicius.zoqueti@raizen.com>
2023-05-04 22:22:07 -0300

    Configuração para navegação entre módulos

--- app/src/main/java/com/br/topfilms/ui/MainActivity.kt
+++ app/src/main/java/com/br/topfilms/ui/MainActivity.kt
@@ -2,7 +2,7 @@ package com.br.topfilms.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.topfilms.R
+import com.br.topfilms.R
 
 class MainActivity : AppCompatActivity() {
 
